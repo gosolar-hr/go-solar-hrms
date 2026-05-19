fffb0bc7a35c86d4fb9b8fcefc55ae4ea649c04c
gosolar-hr <hr@gosolar.co.in>
2026-05-20 01:23:37 +0530

    feat(api): expand employee update handler to accept 24 new master fields

--- pages/api/employees/[id].js
+++ pages/api/employees/[id].js
@@ -26,6 +26,17 @@ export default async function handler(req, res) {
       pan, aadhaar, bank_account,
       ifsc_code, bank_branch, bank_location,
       uan_number, pf_number, emp_code, work_schedule,
+      // New fields from master data
+      date_of_birth, marital_status, father_husband_name,
+      biometric_code, working_location,
+      current_address, permanent_address,
+      aadhaar_name, bank_account_name, pf_basic_limit,
+      prev_uan_number, prev_pf_number, prev_pension_member,
+      prev_pf_action, prev_esic_number,
+      nominee_name, nominee_relation, nominee_phone,
+      nominee_aadhaar, nominee_pan,
+      nominee_current_address, nominee_permanent_address,
+      current_inhand_salary, hr_remark,
     } = req.body
 
     const { data, error } = await supabaseAdmin
@@ -56,6 +67,31 @@ export default async function handler(req, res) {
         uan_number     : uan_number     || null,
         pf_number      : pf_number      || null,
         emp_code       : emp_code       || null,
+        // New fields
+        date_of_birth          : date_of_birth          || null,
+        marital_status         : marital_status         || null,
+        father_husband_name    : father_husband_name    || null,
+        biometric_code         : biometric_code         || null,
+        working_location       : working_location       || null,
+        current_address        : current_address        || null,
+        permanent_address      : permanent_address      || null,
+        aadhaar_name           : aadhaar_name           || null,
+        bank_account_name      : bank_account_name      || null,
+        pf_basic_limit         : pf_basic_limit ? Number(pf_basic_limit) : 15000,
+        prev_uan_number        : prev_uan_number        || null,
+        prev_pf_number         : prev_pf_number         || null,
+        prev_pension_member    : prev_pension_member    || null,
+        prev_pf_action         : prev_pf_action         || null,
+        prev_esic_number       : prev_esic_number       || null,
+        nominee_name           : nominee_name           || null,
+        nominee_relation       : nominee_relation       || null,
+        nominee_phone          : nominee_phone          || null,
+        nominee_aadhaar        : nominee_aadhaar        || null,
+        nominee_pan            : nominee_pan            || null,
+        nominee_current_address   : nominee_current_address   || null,
+        nominee_permanent_address : nominee_permanent_address || null,
+        current_inhand_salary  : current_inhand_salary ? Number(current_inhand_salary) : null,
+        hr_remark              : hr_remark              || null,
       })
       .eq('id', id)
       .select()
